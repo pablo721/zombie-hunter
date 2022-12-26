c1d1e3a1340e0395608da5dd25194439ecb674a2
pablo <pablo774@protonmail.com>
2022-12-26 21:33:32 +0100

    initial

---
+++ hunter.py
@@ -0,0 +1,35 @@
+import simfin as sf
+import pandas as pd
+
+
+class ZombieHunter:
+    def __init__(self):
+        key = 'p84KvYhpzCeiudDOdfK21aHA8IRx7sYC'
+        sf.set_api_key(key)
+        sf.set_data_dir('~/simfin_data/')
+
+
+    @staticmethod
+    def find_zombies(market):
+        df = sf.load_income(variant='annual', market=market)
+        df = df[df['Interest Expense, Net'] > 0]
+        df['Zombie Indicator'] = df['Operating Income (Loss)'] / df['Interest Expense, Net']
+        df = df[df['Zombie Indicator'] < 1]
+        return df
+
+
+    @staticmethod
+    def rank_zombies(df):
+        return df.sort_values(by='Zombie Indicator')
+
+
+    def hunt_zombies(self, market):
+        zombies = self.find_zombies(market)
+        zombies.to_csv('zombies.csv')
+        ranked_zombies = self.rank_zombies(zombies)
+        ranked_zombies.to_csv('ranked_zombies.csv')
+
+
+if __name__ == '__main__':
+    hunter = ZombieHunter()
+    hunter.hunt_zombies('us')
